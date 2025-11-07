fedff05cd93a04848a923d596e754487a08693f3
Tushar Nagle <140832814+tushar93026@users.noreply.github.com>
2025-11-07 12:40:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ connectCloudinary()
 
 // middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: ['https://frontend-updated-eight.vercel.app', 'https://admin-doctor.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 // api endpoints
 app.use("/api/user", userRouter)
